edb7d7f3a6cdd6faa4f618b38568f83fef9a3731
Simon Ljungberg <hi@iamsim.me>
2013-09-17 11:38:25 -0400

    Mega commit. Implemented hashing and wrapping of the memcached API.

--- index.js
+++ index.js
@@ -4,12 +4,37 @@
 // Super simple memcached client tailored to our needs at Bloglovin.
 //
 
-var net = require('net');
-var _   = require('lodash');
+var _     = require('lodash');
+var mc    = require('memcached');
+var crc32 = require('buffer-crc32');
 
 var Memcached = module.exports = function memcached(options) {
+  // Set default options
   options = typeof options === 'object' ? options : {};
-  this.servers = this.serverRing(options.servers || { '127.0.0.1:11211': 1 });
+  options.servers = options.servers || { '127.0.0.1:11211': 1 };
+
+  // Setup connections and hashrings
+  var config     = options.config || {};
+  this.sumWeight = 0;
+  this.conns     = this.setupConnections(Object.keys(options.servers), config);
+  this.servers   = this.serverRing(options.servers);
+};
+
+//
+// ## Setup connections
+//
+// Opens connections to each server.
+//
+// * **servers**, _an array of IP:ports._
+//
+// **Returns** an object with connections keyed to ip.
+//
+Memcached.prototype.setupConnections = function setupConnections(servers, config) {
+  var connections = {};
+  _.map(servers, function (server) {
+    connections[server] = new mc(server, config);
+  });
+  return connections;
 };
 
 //
@@ -49,6 +74,7 @@ var Memcached = module.exports = function memcached(options) {
 Memcached.prototype.serverRing = function serverRing(servers) {
   var hashRing    = [];
   var serverCount = _.reduce(servers, function (t, w) { return t + w; }, 0);
+  this.sumWeight  = serverCount;
   function addToRing(weight, ip) {
     hashRing.push(ip);
     if (--servers[ip] < 1) {
@@ -61,4 +87,36 @@ Memcached.prototype.serverRing = function serverRing(servers) {
 
   return hashRing;
 };
+
+//
+// ## Hash key
+//
+// Hashes the key and returns the correct server.
+//
+// * **key**, _a key to hash._
+//
+// **Returns** a memcached connection object.
+//
+Memcached.prototype.hashKey = function hashKey(key) {
+  var checksum = crc32.unsigned(key);
+  var index    = checksum % this.sumWeight;
+  var server   = this.servers[index];
+  return this.conns[server];
+};
+
+//
+// ## Simulate the memcached module's API
+//
+// Dynamically create prototype methods for the memcached API.
+//
+var methods = ['touch', 'get', 'gets', 'getMulti', 'set', 'replace', 'add',
+    'cas', 'append', 'prepend', 'incr', 'decr', 'del'];
+_.map(methods, function (method) {
+  Memcached.prototype[method] = function (method) {
+    return function () {
+      var server = this.hashKey(arguments[0]);
+      server.apply(server, arguments);
+    };
+  }(method);
+});
 
--- test/mc.js
+++ test/mc.js
@@ -5,9 +5,9 @@
 var assert = require('assert');
 var mc     = require('../');
 
-suite('Memcached Lite', function () {
+suite('BL Memcached Sharder', function () {
 
-  test ('Correctly generates hashring', function () {
+  test('Correctly generates hashring', function () {
     var m = new mc();
 
     var a = m.serverRing({
@@ -29,6 +29,33 @@ suite('Memcached Lite', function () {
       '1.1.1.2',
     ];
     assert.deepEqual(a, a_expected);
+  });
+
+  test('Correctly creates server connections', function () {
+    var m = new mc({
+      servers: {
+        '1.1.1.1:11211': 1,
+        '1.1.1.2:11211': 1
+      }
+    });
+
+    assert.equal(m.conns['1.1.1.1:11211'].servers[0], '1.1.1.1:11211');
+    assert.equal(m.conns['1.1.1.2:11211'].servers[0], '1.1.1.2:11211');
+  });
+
+  test('Hashing key works', function () {
+    var m = new mc();
+    var server = m.hashKey('foobar');
+    assert.equal(typeof server, 'object');
+  });
+
+  test('Dynamic API wrappers correctly setup', function () {
+    var methods = ['touch', 'get', 'gets', 'getMulti', 'set', 'replace', 'add',
+        'cas', 'append', 'prepend', 'incr', 'decr', 'del'];
+    var m = new mc();
+    methods.map(function (method) {
+      assert.equal(typeof m[method], 'function');
+    });
   });
 });
 
